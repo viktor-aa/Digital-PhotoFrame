6f776f9fdcc96029b0137b833ad08c3034b7cde8
Viktor Jovanovski <viktor.jovanovski@yoc.com>
2026-08-11 18:13:39 +0200

    - v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 28
         targetSdk = 37
         versionCode = 1
-        versionName = "0.0.1"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
