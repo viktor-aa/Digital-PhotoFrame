8b3a67710cd5761f24a3704623de50e73fb2a41f
Viktor Jovanovski <viktor.jovanovski@yoc.com>
2026-08-15 07:54:34 +0200

    - v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "mk.amazingapps.digitalphotoframe"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
